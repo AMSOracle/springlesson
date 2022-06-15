e10eb9419dbc5d345c8699a8fc8e3fcea5ff9ae9
Mabdullaev <mabdullaev@sportmaster.ru>
2022-06-15 14:07:53 +0300

    lesson 8+9 Thymeleaf + Rest API part 1

--- lesson7/src/main/java/com/mabdullaev/lesson7/controllers/ProductController.java
+++ lesson7/src/main/java/com/mabdullaev/lesson7/controllers/ProductController.java
@@ -4,6 +4,9 @@ import com.mabdullaev.lesson7.model.dao.Product;
 import com.mabdullaev.lesson7.model.dto.ProductDto;
 import com.mabdullaev.lesson7.services.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,29 +18,35 @@ public class ProductController {
 
     private final ProductService productService;
 
+
     @GetMapping
-    public List<Product> findAll(){
-        return productService.findAll();
+    public Page<ProductDto> findAll(@RequestParam(name = "p", defaultValue = "1") int pageIndex) {
+        if (pageIndex < 1) {
+            pageIndex = 1;
+        }
+        return productService.findAll(pageIndex - 1, 10);
     }
 
     @GetMapping("/{id}")
-    public Product findById(@PathVariable Long id){
+    public ProductDto findById(@PathVariable Long id) {
         return productService.findById(id);
     }
 
     @PostMapping
-    public void save(@RequestBody ProductDto productDto){
-        productService.save(productDto);
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductDto createProduct(@RequestBody ProductDto productDto) {
+        return productService.create(productDto);
     }
 
-    @PostMapping("/delete/{id}")
-    public void deleteById(Long id){
+    @PutMapping
+    public ProductDto updateProduct(@RequestBody ProductDto productDto) {
+        return productService.upd(productDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id) {
         productService.deleteById(id);
     }
 
-    @GetMapping("/filterByPrice")
-    public List<Product> filterByPrice(@RequestParam(required = false) Integer min, @RequestParam(required = false) Integer max){
-        return productService.filterByPrice(min,max);
-    }
 
 }
---
+++ lesson7/src/main/java/com/mabdullaev/lesson7/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.mabdullaev.lesson7.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<?> catchRuntimeException(RuntimeException e){
+        return ResponseEntity.badRequest().body(new MarketError(e.getMessage(), Arrays.stream(e.getStackTrace()).map(el -> el.toString()).collect(Collectors.joining("; "))));
+    }
+}
